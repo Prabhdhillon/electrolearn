37c4a17e80cf0b4262c8d429d9b9cb283a90f997
Prabhdhillon <73169414+Prabhdhillon@users.noreply.github.com>
2021-04-18 15:48:32 +0530

    Resolved Merge Conflicts

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,10 @@
 <?php
 
-<<<<<<< Updated upstream
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\Course;
-=======
 use App\Http\Controllers\CatalogueController;
 use App\Http\Controllers\InstructorController;
 use App\Http\Controllers\RegisterController;
->>>>>>> Stashed changes
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\HomeController;
 
@@ -25,7 +22,6 @@ use App\Http\Controllers\Admin\HomeController;
 Route::get("/", function(){
   return redirect("/admin/home");
 });
-<<<<<<< Updated upstream
 
 Route::get('/login', [AuthController::class, 'login'])->name("login");
 Route::post('/login', [AuthController::class, 'handleLogin']);
@@ -41,8 +37,6 @@ Route::get("/admin/courses/{course}", HomeController::class);
 Route::get("/admin/courses/{course}/upload", HomeController::class);
 Route::get("/admin/change-password", HomeController::class);
 Route::get("/admin/signout", HomeController::class);
-=======
-Route::get('/register', [RegisterController::class, 'register'])->name("register");
+
 Route::get('/instructor', [InstructorController::class, 'instructor'])->name("instructor");
 Route::get('/catalogue', [CatalogueController::class, 'catalogue'])->name("catalogue");
->>>>>>> Stashed changes
