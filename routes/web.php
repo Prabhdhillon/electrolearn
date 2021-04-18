4d7eb0c0a34e2195e47688cc9b977075aaecec76
Prabhdhillon <73169414+Prabhdhillon@users.noreply.github.com>
2021-04-18 15:39:56 +0530

    Added Design Files

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+<<<<<<< Updated upstream
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\Course;
+=======
+use App\Http\Controllers\CatalogueController;
+use App\Http\Controllers\InstructorController;
+use App\Http\Controllers\RegisterController;
+>>>>>>> Stashed changes
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\HomeController;
 
@@ -19,6 +25,7 @@ use App\Http\Controllers\Admin\HomeController;
 Route::get("/", function(){
   return redirect("/admin/home");
 });
+<<<<<<< Updated upstream
 
 Route::get('/login', [AuthController::class, 'login'])->name("login");
 Route::post('/login', [AuthController::class, 'handleLogin']);
@@ -34,3 +41,8 @@ Route::get("/admin/courses/{course}", HomeController::class);
 Route::get("/admin/courses/{course}/upload", HomeController::class);
 Route::get("/admin/change-password", HomeController::class);
 Route::get("/admin/signout", HomeController::class);
+=======
+Route::get('/register', [RegisterController::class, 'register'])->name("register");
+Route::get('/instructor', [InstructorController::class, 'instructor'])->name("instructor");
+Route::get('/catalogue', [CatalogueController::class, 'catalogue'])->name("catalogue");
+>>>>>>> Stashed changes
