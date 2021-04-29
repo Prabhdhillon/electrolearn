35ccf22d05bf1b980a1880f61f7d709b1abd296b
GamingRiot <51694567+GamingRiot@users.noreply.github.com>
2021-04-29 22:35:06 +0530

    Update allcourses

--- resources/views/admin/allcourses.blade.php
+++ resources/views/admin/allcourses.blade.php
@@ -11,15 +11,30 @@
     <div class="container mt-5">
         @forelse ($courses as $course)
             <section class="row mb-5">
-                <div class="col-md-4"><img src="/{{ $course->thumbnail }}" class="img-screen" alt=""></div>
-                <div class="col-md-8">
-                    <h3>{{ $course->title }}</h3>
-
-                    <h5>By {{ auth()->user()->name }}</h5>
-                    <p style="word-break: break-all;font-size:18px;">{{ $course->description }}</p>
-                    <a href="/admin/course/{{ $course->slug }}/videos" class="btn btn-primary">Videos</a>
-                    <a href="/admin/course/{{ $course->slug }}/videos/edit" class="btn btn-warning">Edit</a>
-                    <a href="/admin/course/{{ $course->slug }}/videos" class="btn btn-danger">Delete</a>
+                <div class="col-md-6">
+                    <a class="block block-rounded block-transparent d-md-flex align-items-md-stretch bg-image"
+                        style="background-image: url('assets/media/photos/photo24.jpg');"
+                        href="be_pages_education_course.html" data-toggle="click-ripple">
+                        <div class="block-content block-content-full bg-black-50">
+                            <span
+                                class="d-inline-block py-1 px-2 rounded bg-black-75 font-size-sm font-w700 text-uppercase text-white">
+                                JavaScript
+                            </span>
+                            <div class="py-6">
+                                <div class="font-size-sm mb-2">
+                                    <i class="fa fa-star text-warning"></i>
+                                    <i class="fa fa-star text-warning"></i>
+                                    <i class="fa fa-star text-warning"></i>
+                                    <i class="fa fa-star text-warning"></i>
+                                    <i class="fa fa-star text-warning"></i>
+                                </div>
+                                <h3 class="font-w700 text-white mb-1">Learn Vue 2: Step By Step</h3>
+                            </div>
+                            <span class="font-size-sm font-w700 text-uppercase text-white-75">
+                                35 Lessons | Beginner
+                            </span>
+                        </div>
+                    </a>
                 </div>
             </section>
         @empty
