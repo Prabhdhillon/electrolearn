aa8dae9653ed67a2bb8510f7bf3bd3aabafad92d
GamingRiot <51694567+GamingRiot@users.noreply.github.com>
2021-04-30 06:26:06 +0530

    Uploads folder recovered

--- resources/views/admin/allcourses.blade.php
+++ resources/views/admin/allcourses.blade.php
@@ -11,28 +11,17 @@
     <div class="container mt-5">
         @forelse ($courses as $course)
             <section class="row mb-5">
-                <div class="col-md-6">
+                <div class="col-md-5">
                     <a class="block block-rounded block-transparent d-md-flex align-items-md-stretch bg-image"
-                        style="background-image: url('assets/media/photos/photo24.jpg');"
-                        href="be_pages_education_course.html" data-toggle="click-ripple">
+                        href="/admin/course/{{ $course->slug }}/videos" data-toggle="click-ripple"><img
+                            src="/{{ $course->thumbnail }}" alt="">
                         <div class="block-content block-content-full bg-black-50">
-                            <span
-                                class="d-inline-block py-1 px-2 rounded bg-black-75 font-size-sm font-w700 text-uppercase text-white">
-                                JavaScript
-                            </span>
+
                             <div class="py-6">
-                                <div class="font-size-sm mb-2">
-                                    <i class="fa fa-star text-warning"></i>
-                                    <i class="fa fa-star text-warning"></i>
-                                    <i class="fa fa-star text-warning"></i>
-                                    <i class="fa fa-star text-warning"></i>
-                                    <i class="fa fa-star text-warning"></i>
-                                </div>
-                                <h3 class="font-w700 text-white mb-1">Learn Vue 2: Step By Step</h3>
+
+                                <h3 class="font-w700 text-white mb-1">{{ $course->title }}</h3>
                             </div>
-                            <span class="font-size-sm font-w700 text-uppercase text-white-75">
-                                35 Lessons | Beginner
-                            </span>
+
                         </div>
                     </a>
                 </div>
@@ -41,7 +30,9 @@
             <h1>Sorry there are no courses!</h1>
         @endforelse
     </div>
-
+    {{-- <a href="/admin/course/{{ $course->slug }}/videos" class="btn btn-primary">Videos</a>
+    <a href="/admin/course/{{ $course->slug }}/videos/edit" class="btn btn-warning">Edit</a>
+    <a href="/admin/course/{{ $course->slug }}/videos" class="btn btn-danger">Delete</a> --}}
 
 
 
--- resources/views/admin/video/index.blade.php
+++ resources/views/admin/video/index.blade.php
@@ -4,6 +4,45 @@
     <div class="container">
         <h2> {{ $course->title }}</h2>
         <div class="font-w600" style="font-size:24px;">Videos:</div>
+        @forelse ($videos as $video)
+            <div class="block block-rounded block-bordered mt-4">
+                <div class="block-content">
+                    <table class="table table-striped table-borderless table-vcenter">
+                        <tbody>
+                            <tr>
+                                <td class="text-center w-25 d-none d-md-table-cell">
+                                    <a class="item item-circle bg-primary text-white font-size-h2 mx-auto"
+                                        href="javascript:void(0)">
+                                        {{ $loop->index + 1 }}
+                                    </a>
+                                </td>
+                                <td>
+                                    <div class="py-4">
+
+                                        <a class="link-fx h4 mb-2 d-inline-block text-dark" href="javascript:void(0)">
+                                            {{ $video->title }}
+                                        </a>
+                                        <p class="text-muted mb-0">
+                                            Ligula hendrerit nibh, ac cursus nibh sapien in purus. Mauris tincidunt
+                                            tincidunt
+                                            turpis in porta. Integer fermentum tincidunt auctor. Vestibulum ullamcorper,
+                                            odio
+                                            sed rhoncus imperdiet, enim elit sollicitudin orci, eget dictum leo mi nec
+                                            lectus.
+                                        </p>
+                                    </div>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        @empty
+
+        @endforelse
+
+
+
     </div>
 
 @endsection
